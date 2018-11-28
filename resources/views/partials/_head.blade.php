94a1bced6d6f93de87dfaf6cd650783b4f8b21a8
reakylegabriel <reakylegabriel@gmail.com>
2018-11-28 17:10:18 +0800

    add loading bar

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -8,4 +8,28 @@
     <link href="/font-awesome/css/font-awesome.css" rel="stylesheet">
     <link href="/argon-design/assets/css/argon.css" rel="stylesheet">
     <link href="/DataTables/Data/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <style>
+        .pace {
+          -webkit-pointer-events: none;
+          pointer-events: none;
+
+          -webkit-user-select: none;
+          -moz-user-select: none;
+          user-select: none;
+        }
+
+        .pace-inactive {
+          display: none;
+        }
+
+        .pace .pace-progress {
+          background: #4000ff;
+          position: fixed;
+          z-index: 2000;
+          top: 0;
+          right: 100%;
+          width: 100%;
+          height: 2px;
+        }
+    </style>
     @yield('stylesheets')
--- resources/views/partials/_script.blade.php
+++ resources/views/partials/_script.blade.php
@@ -10,9 +10,19 @@
     <script src="/argon-design/assets/js/argon.js"></script>
     <script src="/jQuery-Mask/dist/jquery.mask.js"></script>
     <script src="/jquery/additional-methods.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pace/1.0.2/pace.js"></script>
     <script src="/js/_script.js"></script>
     @yield('scripts')
     <script>
+            paceOptions = {
+              // Disable the 'elements' source
+              elements: false,
+
+              // Only show the progress on regular and ajax-y page navigation,
+              // not every request
+              restartOnRequestAfter: false,
+            }
+
             @if(Session::has('success'))
                 toastr.success('{{ Session::get('success') }}');
             @endif
